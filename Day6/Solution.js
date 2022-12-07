25b8ad3ca47b1e3e8436dabcf00d06c5e58a5e28
I0hannes <97alfredo@live.it>
2022-12-07 21:25:58 +0100

    Added the second solution for the sixth day

--- Day6/Solution.js
+++ Day6/Solution.js
@@ -3,23 +3,34 @@ var input = `rdzrddbgdbbqtbqbrrznznjzjjctcbtttrvrwwsvwssjfjcjnjzzqgzgzsggfddvnnr
  // #region First Part
  var firstPart = () => {
     var count = 0
+    var value = 4
     for (let index = 0; index < input.length; index++) {
-        var nRepetition = input.slice(index,index+4).match(/(.).*\1/)
+        var nRepetition = input.slice(index,index + value).match(/(.).*\1/)
         if(nRepetition?.length) continue
         if(!nRepetition){
-            count = index+4
+            count = index+value
             break
         } 
     }
 
     console.log("first part", count)
- 
 }
  // #endregion
 
  // #region Second Part
  var secondPart = () => {
+    var count = 0
+    var value = 14
+    for (let index = 0; index < input.length; index++) {
+        var nRepetition = input.slice(index,index + value).match(/(.).*\1/)
+        if(nRepetition?.length) continue
+        if(!nRepetition){
+            count = index+value
+            break
+        } 
+    }
 
+    console.log("second part", count)
 }
  // #endregion
 
