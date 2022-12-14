c6a1a56a220f5c80e9f6ec52791604008dad8115
I0hannes <97alfredo@live.it>
2022-12-14 15:53:06 +0100

    Better Version of parser

--- Day7/Solution.js
+++ Day7/Solution.js
@@ -14,9 +14,9 @@ rows.forEach((x,i) => {
     let isInstruction = x.startsWith("$")
     let isFile = /(^(\d)*)\s(\w)+/.test(x)
     let isFolder = /^(dir\s(\w)+).*/.test(x)
-    temp.push({value: x, index: i, isInstruction: isInstruction, isFile: isFile, isFolder: isFolder})
+    let isList = /(ls)$/.test(x)
+    temp.push({value: x, index: i, isInstruction: isInstruction, isFile: isFile, isFolder: isFolder, isList: isList})
 })
-
 
 var findNextInstruction = (index) =>{
       return temp.filter((x, i)=>{
@@ -35,65 +35,37 @@ var positionOf = (element) => {
 }
 
 var array = []
-// TODO: using the index of the row get the current instruction and then skip the usewd results
-temp.forEach((x,i)=>{
-    if(x.isInstruction){
-        if(x.value.includes("cd")){
-            // x -> go to currFolder
-            // .. -> enter parent currFolder
-            // / -> go to root
 
-            if(x.value.includes("/")){
-                if(result.length === 0)
-                {
-                    result.push({hasParent: false, parentIndex: 0, index: i, name: "\\", isFile: false})
-                    console.log("folder start /")
-                }
-                if(result.length != 0) 
-                    currFolder = result.find((x) => {
-                        return x.name === "/"
-                })
-            }
+console.log("Next instruction", nextInstrucion)
+for (let index = 0; index <  temp.length; index++) {
+    var nextInstrucion = findNextInstruction(index)
+    const element = temp[index]
+    // if(!element.isFile || !element.isFolder) continue
 
-            if(x.value.includes("..")){
-                // TODO: take the current name of currFolder and returnn the parent of it
-                // result.find(x => x.name === currFolder.name)
-                console.log("change the current folder to the parent folder")
-            }
+    let parent = findParentFolder(index)
+    let values = element.value.split(" ")
+    let sizeFile = values[0] 
+    let nameFile = values[1]
+    let nameFolder = values[1]
+    
 
-            if (x.value.split(" ")[2]) {
-                // TODO: search and find for each nested array
-                // result.find()
-                console.log(`add new name folder: ${x.value.split(" ")[2]}`)
-            }
-        }
+    let object = {}
+    object.parent = parent
+    object.index = index 
 
-        if(x.value.includes("ls")){
-            // TODO: find the next instrucion to use it as reference to take all the rows
-
-            var arrTemp = []
-            console.log("Next instruction", nextInstrucion)
-            for (let index = 0; index <  temp.length; index++) {
-                var nextInstrucion = findNextInstruction(i)
-                const element = temp[index];
-                if(!element.isFile) continue
-
-                let parent = findParentFolder(index)
-                let values = element.value.split(" ")
-                let size =  values[0] 
-                let name = values[1]
-                
-                array.push({parent: parent, index: index, name: name, size: size, isFile: element.isFile, isFolder: element.isFolder })
-                console.log(arrTemp)
-                // console.log(element, parent)
-                if(nextInstrucion?.index === index){
-                    break
-                }
-            }
-
-        }
-    } 
-})
+    if(element.isFolder) {
+        object.name = nameFolder
+        object.size = undefined
+        object.isFolder = true
+    }
+    if(element.isFile) {
+        object.name = nameFile
+        object.size = sizeFile
+        object.isFile = true
+    }
+    array.push(object)
+    // console.log(values, object)
+}
  // #endregion
 
 
