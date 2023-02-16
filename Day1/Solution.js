0c4deb739661db6b54a285350e278c93f0a6cccb
I0hannes <>
2023-02-16 18:52:30 +0100

    Day1 retrieved

---
+++ Day1/Solution.js
@@ -0,0 +1,32 @@
+readTextFile("Input.txt")
+
+var goblinsCalories = input.split("\n\n")
+
+var goblins = []
+
+goblinsCalories.forEach ((goblin) =>{
+  let calories = goblin.split("\n").map (x => parseInt(x,10))
+
+  let eachTot = calories.reduce((acc, curr) => acc + curr, 0)
+
+  goblins.push(eachTot)
+})
+
+// console.log(goblins);
+
+var max = Math.max(...goblins);
+
+// console.log(max)
+
+goblins = goblins.filter((x) => { return x !== max});
+
+var max2 = Math.max(...goblins);
+
+goblins = goblins.filter((x) => { return x !== max2});
+
+var max3 = Math.max(...goblins);
+
+var finalResult = max + max2 + max3;
+
+console.log("max", max, "max2", max2, "max3", max3)
+console.log("finale", finalResult)
