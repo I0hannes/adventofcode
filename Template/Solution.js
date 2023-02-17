a5db5ece970890f80bd69d1fd05ce48d79be280f
I0hannes <97alfredo@live.it>
2023-02-17 07:03:12 +0100

    Adding more sections for the Template

--- Template/Solution.js
+++ Template/Solution.js
@@ -1,1 +1,23 @@
 readTextFile("inputTest.txt")
+
+
+ // #region Parse Input
+ 
+ // #endregion
+
+ // #region First Part
+ var firstPart = () => {
+ 
+ }
+  // #endregion
+ 
+  // #region Second Part
+  var secondPart = () => {
+ 
+ }
+  // #endregion
+ 
+  // #region Resolution
+  firstPart()
+  secondPart()
+  // #endregion
