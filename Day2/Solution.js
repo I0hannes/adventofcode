3486eef88be63f7a853a0dd125bf25c209b4d972
I0hannes <97alfredo@live.it>
2023-02-18 17:57:02 +0100

    Day2 Rearranged

--- Day2/Solution.js
+++ Day2/Solution.js
@@ -1,85 +1,4 @@
-readTextFile("input.txt")
-
-
-var legend = [
-    { name: "Rock", value: 1, letter: "AX", win: "CZ" },
-    { name: "Paper", value: 2, letter: "BY", win: "AX" },
-    { name: "Scissors", value: 3, letter: "CZ", win: "BY" }
-];
-
-var matrix = input.split("\n");
-var list = [];
-var score = 0;
-
-matrix.forEach((ele) => {
-    var fight = new Array(ele.split(" "));
-    var result = [];
-
-    if (fight[0].length === 1) {
-        return;
-    }
-
-    var enemy =
-        fight[0][0] === "A"
-            ? 1
-            : fight[0][0] === "B"
-                ? 2
-                : fight[0][0] === "C"
-                    ? 3
-                    : 0;
-    var mine =
-        fight[0][1] === "X"
-            ? 1
-            : fight[0][1] === "Y"
-                ? 2
-                : fight[0][1] === "Z"
-                    ? 3
-                    : 0;
-
-    var isDraw = false;
-    var isWon = false;
-    if (mine === enemy) isDraw = true;
-    else {
-        switch (mine) {
-            case 1:
-                isWon = enemy === 3;
-                break;
-            case 2:
-                isWon = enemy === 1;
-                break;
-            case 3:
-                isWon = enemy === 2;
-                break;
-            default:
-                console.log("1");
-                break;
-        }
-    }
-
-    var score = mine + (isWon ? 6 : 0) + (isDraw ? 3 : 0);
-    // console.log(enemy, mine, isWon, isDraw, score)
-    list.push(score);
-    // console.log("prova", result.enemy)
-    // result.mine = legend.letter.includes(mine).value
-    // result.draw = result.enemy === result.mine
-    // result.wins = legend.find(x => x.letter.includes(mine)).win.includes("")
-    // result.score = result.mine + (result.draw ? 3 : 0) + (result.wins ? 6 : 0)
-    // console.log(result)
-    //list.push(result)
-});
-
-console.log(list.reduce((acc, curr) => acc + curr, 0));
-
-readTextFile("input.txt")
-
-var legend = [
-    { name: "Rock", value: 1, letter: "AX", win: "CZ" },
-    { name: "Paper", value: 2, letter: "BY", win: "AX" },
-    { name: "Scissors", value: 3, letter: "CZ", win: "BY" }
-];
-var matrix = input.split("\n");
-var list = [];
-
+// #region Functions
 var findWinnnerAgainst = (competitor) => {
     switch (competitor) {
         case 1:
@@ -107,65 +26,142 @@ var findLoserAgainst = (competitor) => {
             break;
     }
 }
+// #endregion
 
-matrix.forEach((ele) => {
-    var fight = new Array(ele.split(" "));
+// #region First Part
+var firstPart = () => {
+    readTextFile("input.txt")
+    var legend = [
+        { name: "Rock", value: 1, letter: "AX", win: "CZ" },
+        { name: "Paper", value: 2, letter: "BY", win: "AX" },
+        { name: "Scissors", value: 3, letter: "CZ", win: "BY" }
+    ];
 
-    if (fight[0].length === 1) {
-        return;
-    }
+    var matrix = input.split("\n");
+    var list = [];
+    var score = 0;
+    matrix.forEach((ele) => {
+        var fight = new Array(ele.split(" "));
 
-    var enemy =
-        fight[0][0] === "A"
-            ? 1
-            : fight[0][0] === "B"
-                ? 2
-                : fight[0][0] === "C"
-                    ? 3
-                    : 0;
-
-    var mine =
-        fight[0][1] === "X"
-            ? findLoserAgainst(enemy)
-            : fight[0][1] === "Y"
-                ? enemy
-                : fight[0][1] === "Z"
-                    ? findWinnnerAgainst(enemy)
-                    : 0;
-
-    var isDraw = false;
-    var isWon = false;
-    if (mine === enemy) isDraw = true;
-    else {
-        switch (mine) {
-            case 1:
-                isWon = enemy === 3;
-                break;
-            case 2:
-                isWon = enemy === 1;
-                break;
-            case 3:
-                isWon = enemy === 2;
-                break;
-            default:
-                console.log("1");
-                break;
+        if (fight[0].length === 1) {
+            return;
         }
-    }
 
-    var score = mine + (isWon ? 6 : 0) + (isDraw ? 3 : 0);
-    // console.log(enemy, mine, isWon, isDraw, score)
-    list.push(score);
-    // console.log("prova", result.enemy)
-    // result.mine = legend.letter.includes(mine).value
-    // result.draw = result.enemy === result.mine
-    // result.wins = legend.find(x => x.letter.includes(mine)).win.includes("")
-    // result.score = result.mine + (result.draw ? 3 : 0) + (result.wins ? 6 : 0)
-    // console.log(result)
-    //list.push(result)
-});
+        var enemy =
+            fight[0][0] === "A"
+                ? 1
+                : fight[0][0] === "B"
+                    ? 2
+                    : fight[0][0] === "C"
+                        ? 3
+                        : 0;
+        var mine =
+            fight[0][1] === "X"
+                ? 1
+                : fight[0][1] === "Y"
+                    ? 2
+                    : fight[0][1] === "Z"
+                        ? 3
+                        : 0;
 
-console.log(list.reduce((acc, curr) => acc + curr, 0));
+        var isDraw = false;
+        var isWon = false;
+        if (mine === enemy) isDraw = true;
+        else {
+            switch (mine) {
+                case 1:
+                    isWon = enemy === 3;
+                    break;
+                case 2:
+                    isWon = enemy === 1;
+                    break;
+                case 3:
+                    isWon = enemy === 2;
+                    break;
+                default:
+                    console.log("1");
+                    break;
+            }
+        }
+
+        var score = mine + (isWon ? 6 : 0) + (isDraw ? 3 : 0);
+        list.push(score);
+
+    });
+
+    console.log("First Part:", list.reduce((acc, curr) => acc + curr, 0));
+}
+// #endregion
 
 
-// TODO Rearrange it.
+// #region Second Part
+var secondPart = () => {
+    readTextFile("input.txt")
+
+    var legend = [
+        { name: "Rock", value: 1, letter: "AX", win: "CZ" },
+        { name: "Paper", value: 2, letter: "BY", win: "AX" },
+        { name: "Scissors", value: 3, letter: "CZ", win: "BY" }
+    ];
+
+    var matrix = input.split("\n");
+    var list = [];
+    var score = 0;
+
+    matrix.forEach((ele) => {
+        var fight = new Array(ele.split(" "));
+
+        if (fight[0].length === 1) {
+            return;
+        }
+
+        var enemy =
+            fight[0][0] === "A"
+                ? 1
+                : fight[0][0] === "B"
+                    ? 2
+                    : fight[0][0] === "C"
+                        ? 3
+                        : 0;
+
+        var mine =
+            fight[0][1] === "X"
+                ? findLoserAgainst(enemy)
+                : fight[0][1] === "Y"
+                    ? enemy
+                    : fight[0][1] === "Z"
+                        ? findWinnnerAgainst(enemy)
+                        : 0;
+
+        var isDraw = false;
+        var isWon = false;
+        if (mine === enemy) isDraw = true;
+        else {
+            switch (mine) {
+                case 1:
+                    isWon = enemy === 3;
+                    break;
+                case 2:
+                    isWon = enemy === 1;
+                    break;
+                case 3:
+                    isWon = enemy === 2;
+                    break;
+                default:
+                    console.log("1");
+                    break;
+            }
+        }
+
+        var score = mine + (isWon ? 6 : 0) + (isDraw ? 3 : 0);
+        list.push(score);
+    });
+
+    console.log("Second Part: ", list.reduce((acc, curr) => acc + curr, 0));
+}
+// #endregion
+
+// #region Resolution
+firstPart()
+secondPart()
+// #endregion
