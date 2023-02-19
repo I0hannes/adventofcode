3c59a96132bda99b9c0ff970c7b9709d28173e2f
I0hannes <97alfredo@live.it>
2023-02-19 14:00:59 +0100

    Day4 Both Solutions

--- Day4/Solution.js
+++ Day4/Solution.js
@@ -1,96 +1,70 @@
 readTextFile("input.txt")
 
-
- // #region Parse Input
- 
- // #endregion
-
- // #region First Part
- var firstPart = () => {
- 
- }
-  // #endregion
- 
-  // #region Second Part
-  var secondPart = () => {
- 
- }
-  // #endregion
- 
-  // #region Resolution
-  firstPart()
-  secondPart()
-  // #endregion
-
-
-
+// #region Parse Input
 var cleaningGroup = input.split("\n");
-var result = []
-var isFullyContained = (min,min2,max,max2) => {
-  return (min2 >= min && max2 <= max) || (min >= min2 && max <= max2)
+// #endregion
 
-}
-
-for(var key in cleaningGroup){
-  var groups = cleaningGroup[key].split(",").map(x => x.split("-") )
-
-  let [min, max] = groups[0]
-  let [min2, max2] = groups[1]
-  
-
-
-  var isFC = isFullyContained(parseInt(min), parseInt(min2), parseInt(max), parseInt(max2))
-  // console.log(groups, min, max, min2, max2, isFC)
-  result.push(isFC)
-}
-
-
-console.log(result.filter(x => x === true).length);
-
-// 424 (y)
-
-
-
-var cleaningGroup = input.split("\n");
-var result = [];
-
-// var isFullyContained = (min, min2, max, max2) => {
-//   var sInB = min2 >= min && max2 <= max;
-//   var BIns = min >= min2 && max <= max2;
-
-//   return sInB || BIns;
-//   // 424
-// }
-
+// #region Functions
 var isFullyContained = (pair, pair2) => {
-  var sInB = pair2.min >= pair.min && pair2.max <= pair.max
-  var BIns = pair.min >= pair2.min && pair.max <= pair2.max
+    var sInB = pair2.min >= pair.min && pair2.max <= pair.max
+    var BIns = pair.min >= pair2.min && pair.max <= pair2.max
 
-  return sInB || BIns;
-  // 424
+    return sInB || BIns;
+    // 424
 }
+
+var isFullyContained = (min, min2, max, max2) => {
+    return (min2 >= min && max2 <= max) || (min >= min2 && max <= max2)
+
+}
+
 var isPartiallyContained = (pair, pair2) => {
-  var intersection = pair.min <= pair2.max  && pair2.min <= pair.max
-  return intersection
-  // 804
+    var intersection = pair.min <= pair2.max && pair2.min <= pair.max
+    return intersection
 }
+// #endregion
 
-for (var key in cleaningGroup) {
-  var groups = cleaningGroup[key].split(",").map((pair) => {
-    let [min, max] = pair.split("-").map(Number)
-    return { min: min, max: max }
-  });
-  // console.log(groups);
+// #region First Part
+var firstPart = () => {
+    var result = []
 
-  var isFC = isFullyContained(groups[0], groups[1]) 
-  var isPC =  isPartiallyContained(groups[0], groups[1])
-  // console.log(isFC, isPC)
-  result.push(isFC || isPC)
+    for (var key in cleaningGroup) {
+        var groups = cleaningGroup[key].split(",").map(x => x.split("-"))
+
+        let [min, max] = groups[0]
+        let [min2, max2] = groups[1]
+
+        var isFC = isFullyContained(parseInt(min), parseInt(min2), parseInt(max), parseInt(max2))
+        // console.log(groups, min, max, min2, max2, isFC)
+        result.push(isFC)
+    }
+
+    console.log("First Result: ", result.filter(x => x === true).length);
 }
+// #endregion
 
-console.log(result.filter((x) => x === true).length)
+// #region Second Part
+var secondPart = () => {
+    var result = [];
+    
+    for (var key in cleaningGroup) {
+        var groups = cleaningGroup[key].split(",").map((pair) => {
+            let [min, max] = pair.split("-").map(Number)
+            return { min: min, max: max }
+        });
+        // console.log(groups);
+    
+        var isFC = isFullyContained(groups[0], groups[1])
+        var isPC = isPartiallyContained(groups[0], groups[1])
+        // console.log(isFC, isPC)
+        result.push(isFC || isPC)
+    }
+    
+    console.log("Second Part: ", result.filter((x) => x === true).length)
+}
+// #endregion
 
-
-
-
-// TODO Rearrange it.
+// #region Resolution
+firstPart()
+secondPart()
+// #endregion
